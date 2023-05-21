69e033345a9b6213cbed0c1508800f20e6e8a244
sheldondebra <kofisheldon@gmail.com>
2023-05-21 18:07:43 +0000

    myupdat

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
     <div className="">
-<h2>Welcomew</h2>
+      <h2>Dashboard</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
