57705640385eb75e6cb1bae2326129ee8a05c160
Mansir <mansiraziz93@gmail.com>
2023-06-18 15:50:42 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Home from "./components/Home"
-import { BrowserRouter } from "react-router-dom"
-import "./App.css"
+import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+
+import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+});
